4738a2f9ec706ee52f23ab34a638f08c9ce7a803
ZhenyaMsl <valeera47@gmail.com>
2021-03-19 22:08:55 +0300

    annotate User class

--- src/main/java/diary/User.java
+++ src/main/java/diary/User.java
@@ -1,30 +1,37 @@
 package diary;
 
-public class User {
-    private final String login;
-    private final String password;
-    private final String name;
-    private final String about;
+import javax.annotation.Nonnull;
 
-    public User(String login, String password, String name, String about) {
+public class User {
+    @Nonnull private final String login;
+    @Nonnull private final String password;
+    @Nonnull private final String name;
+    @Nonnull private final String about;
+
+    public User(@Nonnull String login, @Nonnull String password,
+                @Nonnull String name, @Nonnull String about) {
         this.login = login;
         this.password = password;
         this.name = name;
         this.about = about;
     }
 
+    @Nonnull
     public String getLogin() {
         return login;
     }
 
+    @Nonnull
     public String getPassword() {
         return password;
     }
 
+    @Nonnull
     public String getName() {
         return name;
     }
 
+    @Nonnull
     public String getAbout() {
         return about;
     }
