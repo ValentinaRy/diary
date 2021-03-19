34165326302e0b9539e0c6df44cd9807464dbf59
ZhenyaMsl <valeera47@gmail.com>
2021-03-19 22:17:38 +0300

    added hashCode and equals to User class

--- src/main/java/diary/User.java
+++ src/main/java/diary/User.java
@@ -1,6 +1,7 @@
 package diary;
 
 import javax.annotation.Nonnull;
+import java.util.Objects;
 
 public class User {
     @Nonnull private final String login;
@@ -34,5 +35,18 @@ public class User {
     @Nonnull
     public String getAbout() {
         return about;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return login.equals(user.login);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login);
     }
 }
