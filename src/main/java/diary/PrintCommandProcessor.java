2c28b9c86d6b2da1b0a79326c47336b2f4b1791f
ZhenyaMsl <valeera47@gmail.com>
2021-03-20 19:14:42 +0300

    printUserCommand implemented

--- src/main/java/diary/PrintCommandProcessor.java
+++ src/main/java/diary/PrintCommandProcessor.java
@@ -32,7 +32,16 @@ public class PrintCommandProcessor {
     }
 
     private static void printUserCommand(String[] args) {
-        // TODO: implement method
+        checkState(args.length > 3, "Not enough arguments for printing user");
+        String login = args[2];
+        String password = args[3];
+        User user = CmdServer.getUsers().get(login);
+        checkState(user != null && user.getPassword().equals(password), "Wrong login or password");
+        System.out.println("User information");
+        System.out.println("Login: " + user.getLogin());
+        System.out.println("Password: " + user.getPassword());
+        System.out.println("Name: " + user.getName());
+        System.out.println("About: " + user.getAbout());
     }
 
     private static void printDiaryCommand(String[] args) {
