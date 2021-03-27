e5dcd08aff4c8f41eb4584c0f9683d40ce230ef2
ValentinaRy <valeera47@gmail.com>
2021-03-27 20:58:22 +0300

    parse implemented in EntryType

--- src/main/java/diary/entry/EntryType.java
+++ src/main/java/diary/entry/EntryType.java
@@ -1,12 +1,26 @@
 package diary.entry;
 
-enum EntryType {
+public enum EntryType {
     INTEGER("integer"),
     DOUBLE("double"),
     STRING("string");
 
     private final String string;
+
     private EntryType(String string) {
         this.string = string;
+    }
+
+    public String string() {
+        return string;
+    }
+
+    public EntryType parse(String string) {
+        for (EntryType value : values()) {
+            if (value.string().equals(string)) {
+                return value;
+            }
+        }
+        throw new IllegalStateException("No entry type: " + string);
     }
 }
