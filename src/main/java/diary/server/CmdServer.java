6f038fa87eebcba32fe68778ea265a9eb9038d96
ValentinaRy <valeera47@gmail.com>
2021-03-28 11:39:53 +0300

    user and diary logic moved to super class Server

--- src/main/java/diary/server/CmdServer.java
+++ src/main/java/diary/server/CmdServer.java
@@ -1,33 +1,29 @@
 package diary.server;
 
-import diary.*;
-import diary.server.storage.FileStorage;
 import diary.server.storage.Storage;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
 
 import static com.google.common.base.Preconditions.checkState;
 
 public class CmdServer extends Server {
-    private static final Storage storage = new FileStorage("storage/users.txt", "storage/diaries.txt");
-    private static final Map<String, User> users = new HashMap<>();
-    private static final Map<User, Diary> diaryPerUserMap = new HashMap<>();
+    public CmdServer(Storage storage) {
+        super(storage);
+    }
 
-    public static void main(String[] args) {
+    public void start() {
         if (!storage.initLoad(users, diaryPerUserMap)) {
             System.out.println("Error: couldn't initialize server storage. Exiting program");
             return;
         }
         printHelpInfo();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        reader.lines().forEach(CmdServer::processSingleCommandSafe);
+        reader.lines().forEach(this::processSingleCommandSafe);
     }
 
-    private static void printHelpInfo() {
+    private void printHelpInfo() {
         System.out.println("List of all available commands");
         CreateCommandProcessor.printHelpInfo();
         PrintCommandProcessor.printHelpInfo();
@@ -35,7 +31,7 @@ public class CmdServer extends Server {
         System.out.print(">");
     }
 
-    private static void processSingleCommandSafe(String line) {
+    private void processSingleCommandSafe(String line) {
         try {
             processSingleCommand(line);
         } catch (IllegalStateException ex) {
@@ -44,7 +40,7 @@ public class CmdServer extends Server {
         System.out.print(">");
     }
 
-    private static void processSingleCommand(@Nonnull String line) {
+    private void processSingleCommand(@Nonnull String line) {
         String[] args = line.split(" ");
         checkState(args.length > 0, "No arguments were passed");
         switch (args[0]) {
@@ -60,23 +56,5 @@ public class CmdServer extends Server {
             default:
                 throw new IllegalStateException("No such command: " + args[0]);
         }
-    }
-
-    static boolean addUser(@Nonnull User user) {
-        return users.putIfAbsent(user.getLogin(), user) == null;
-    }
-
-    @Nonnull
-    static Map<String, User> getUsers() {
-        return users;
-    }
-
-    static boolean createDiary(@Nonnull User user) {
-        return diaryPerUserMap.putIfAbsent(user, new Diary(user)) == null;
-    }
-
-    @Nullable
-    static Diary getDiary(@Nonnull User user) {
-        return diaryPerUserMap.get(user);
     }
 }
--- src/main/java/diary/server/Server.java
+++ src/main/java/diary/server/Server.java
@@ -1,4 +1,49 @@
 package diary.server;
 
+import diary.Diary;
+import diary.User;
+import diary.server.storage.FileStorage;
+import diary.server.storage.Storage;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Server {
+    @Nonnull protected final Storage storage;
+    @Nonnull protected final Map<String, User> users;
+    @Nonnull protected final Map<User, Diary> diaryPerUserMap;
+
+    public Server(@Nonnull Storage storage) {
+        this.storage = storage;
+        this.users = new HashMap<>();
+        this.diaryPerUserMap = new HashMap<>();
+    }
+
+    public boolean addUser(@Nonnull User user) {
+        return users.putIfAbsent(user.getLogin(), user) == null;
+    }
+
+    @Nonnull
+    public Map<String, User> getUsers() {
+        return users;
+    }
+
+    public boolean createDiary(@Nonnull User user) {
+        return diaryPerUserMap.putIfAbsent(user, new Diary(user)) == null;
+    }
+
+    @Nullable
+    public Diary getDiary(@Nonnull User user) {
+        return diaryPerUserMap.get(user);
+    }
+
+    protected abstract void start();
+
+    public static void main(String[] args) {
+        Storage fileStorage = new FileStorage("storage/users.txt", "storage/diaries.txt");
+        Server server = new CmdServer(fileStorage);
+        server.start();
+    }
 }
