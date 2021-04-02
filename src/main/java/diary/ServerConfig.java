d69eb21aba0899154c4c7a9f35d9ec60d8af972b
ValentinaRy <valeera47@gmail.com>
2021-04-02 20:58:17 +0300

    using property file to get file names

--- src/main/java/diary/ServerConfig.java
+++ src/main/java/diary/ServerConfig.java
@@ -4,13 +4,17 @@ import diary.server.CmdServer;
 import diary.server.Server;
 import diary.server.storage.FileStorage;
 import diary.server.storage.Storage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:diary.properties")
 public class ServerConfig {
     @Bean
-    public Storage storage(String userFile, String diaryFile) {
+    public Storage storage(@Value("${file.user}") String userFile,
+                           @Value("${file.diary}") String diaryFile) {
         return new FileStorage(userFile, diaryFile);
     }
 
