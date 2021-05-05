bfb2e1c10a4a9ab77d12a9ec92cc9fc8b9164991
ValentinaRy <valeera47@gmail.com>
2021-05-05 22:22:43 +0300

    starting with gui for this application!

---
+++ src/main/java/diary/gui/DiaryUI.java
@@ -0,0 +1,21 @@
+package diary.gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class DiaryUI extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label l = new Label("Hello, cool staff will be here soon!");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
